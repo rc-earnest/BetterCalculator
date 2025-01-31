ab1ff8f8f8a6576ec2024b8390fae92e0a3b5157
rc-earnest <rc.earnest04@gmail.com>
2025-01-30 18:50:58 -0700

    Add project files.

---
+++ BetterCalculator/BetterCalculator.vb
@@ -0,0 +1,20 @@
+﻿'Rudy Earnest
+'RCET 2265
+'Spring 2025
+'Better Calculator
+'
+Option Strict On
+Option Explicit On
+Option Compare Text
+Module BetterCalculator
+
+    Sub Main()
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim operation As Integer
+        Dim input As String
+
+
+    End Sub
+
+End Module
