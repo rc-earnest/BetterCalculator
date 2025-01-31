bb09347c085dc263bf0f662d276d17b8c5984186
rc-earnest <rc.earnest04@gmail.com>
2025-01-30 19:16:09 -0700

    added getting first number

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -14,7 +14,27 @@ Module BetterCalculator
         Dim operation As Integer
         Dim input As String
 
+        Do
+            Console.WriteLine("Please enter two whole numbers. Enter ""Q"" at any time to quit.")
 
+            ' Get first number
+            Do
+                Console.Write("Choose Number 1: ")
+                input = Console.ReadLine()
+                If input = "Q" Then
+                    Console.WriteLine("You entered ""q""")
+                    Console.WriteLine("Have a nice day")
+                    Exit Sub
+                End If
+                Try
+                    num1 = CInt(input)
+                    Console.WriteLine($"You entered ""{input}"".")
+                    Exit Do
+                Catch ex As Exception
+                    Console.WriteLine($"You entered ""{input}"" please enter a whole number.")
+                End Try
+            Loop
+        Loop
     End Sub
 
 End Module
